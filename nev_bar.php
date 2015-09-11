99051c993ca1d593ad9564e73d4b7ec94cba9e6c
puuga <siwaoh@gmail.com>
2015-09-11 20:43:35 +0700

    update link and change text

--- nev_bar.php
+++ nev_bar.php
@@ -13,11 +13,12 @@
     </div>
     <div class="navbar-collapse collapse navbar-responsive-collapse">
       <p class="navbar-text">
-        <?php echo $title; ?>
+        <?php echo $title_nev; ?>
       </p>
 
       <ul class="nav navbar-nav navbar-right">
         <li><a href="detail.php"><?php echo $condition; ?></a></li>
+        <li><a href="welcome.php"><?php echo $process_text; ?></a></li>
         <li><a href="#"><?php echo $explanation; ?></a></li>
         <li><a href="#"><?php echo $evaluation; ?></a></li>
       </ul>

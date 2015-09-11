3ef78b48c02d1290a07476be188663a7c8fb37f2
puuga <siwaoh@gmail.com>
2015-09-11 18:29:42 +0700

    change content
    only logo on page

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
 <html lang="th">
 <head>
   <?php include "include_head.php" ?>
-  <title>Home</title>
+  <title><?php echo "$title"; ?></title>
 
   <script>
     $(document).ready(function() {
@@ -13,25 +13,39 @@
 
     });
   </script>
+  <style>
+    .sp-color{
+      background: linear-gradient(to bottom, #FF9800 0%,#FF9800 65%,#000000 65%,#9E9E9E 65%,#9E9E9E 100%); /* W3C */
+    }
+  </style>
 </head>
 <body>
   <div class="container">
-    <h1>Hello Bootstrap</h1>
 
     <div class="row">
 
-      <div class="col-lg-4">
-        <div class="panel panel-info">
-          <div class="panel-heading">
-            <h3 class="panel-title">Panel info</h3>
+      <div class="col-lg-12">
+        <div class="panel panel-material-grey">
+          <div class="panel-body text-center sp-color">
+            <p>
+              <img src="images/nu_logo.png" alt="Naresuan University" />
+            </p>
+            <h1><?php echo $title; ?></h1>
           </div>
-          <div class="panel-body">
-            Panel content
-          </div>
-          <div class="panel-footer">Panel footer</div>
         </div>
       </div>
 
+
+
+    </div>
+
+    <div class="row">
+      <div class="col-lg-6 col-lg-offset-6 text-right">
+        <a class="btn btn-material-orange btn-lg" href="detail.php" role="button">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          Next
+        </a>
+      </div>
     </div>
     <br/>
 

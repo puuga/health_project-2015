3fe5c11209238e27e3b0f7a04d89f191d6cb9d54
puuga <siwaoh@gmail.com>
2015-10-04 14:03:24 +0700

    refactoring code

---
+++ include/function.php
@@ -0,0 +1,15 @@
+<?php
+
+function sectionTitle($message) {
+  return "<div class='row'>
+    <div class='col-lg-12'>
+      <div class='panel panel-material-grey'>
+        <div class='panel-body'>
+          <h2>$message</h2>
+        </div>
+      </div>
+    </div>
+  </div>";
+}
+
+?>
